4eec0d6be68c61ff1a8d8c13ab580555c57bcbfd
lentoo <729533020@qq.com>
2022-12-06 16:38:45 +0800

    feat: 新增source-map、修改completeCopy配置为all

--- test-coverage.config.js
+++ test-coverage.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extension: ['.js', '.ts', '.vue'],
-  completeCopy: true,
+  all: true,
   exclude: ['**/*.d.ts', '**/*.css', '*.scss'],
   reporter: ['html-spa', 'lcov', 'html'],
   reportDir: './report',
