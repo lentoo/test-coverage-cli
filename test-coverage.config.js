7560cf5059e08b1caf85cf4a2848655706ac3d30
李祥彪 <xiangbiao.li@ur.com.cn>
2023-06-12 13:47:59 +0800

    feat: 增加全局对象配置

--- test-coverage.config.js
+++ test-coverage.config.js
@@ -2,11 +2,6 @@ module.exports = {
   extension: ['.js', '.ts', '.vue'],
   all: true,
   exclude: ['**/*.d.ts', '**/*.css', '*.scss'],
-  reporter: ['html-spa', 'lcov', 'html'],
+  reporter: ['html', 'lcov', 'html'],
   reportDir: './report',
-
-  gitlab: {
-    https: false,
-    host: '192.168.13.78',
-  },
 };
